ae93e35e9b89d75477b611357e8d04751e8d9264
Enrico Donelli <e.donelli@myti.it>
2016-12-02 09:52:19 +0100

    url progetto

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject projecteuler "0.1.0-SNAPSHOT"
   :description "project euler in clojure"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/enricod/projecteuler"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
